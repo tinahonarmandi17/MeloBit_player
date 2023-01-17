d01a66183401d3df9916546e06585f9071a26ef0
wotox2 <tinahonarmandi17@gmail.com>
2023-01-17 10:27:35 +0330

    Activity main and Permission checker Completed

---
+++ Music_player_java/app/src/main/java/com/example/musicplaterjava/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.musicplaterjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.musicplaterjava.activity.AppMainPage;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int TAG = 752;
+    private Button goInside ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        goInside = findViewById(R.id.inside);
+        goInside.setOnClickListener(v -> {
+            startActivity(new Intent(this, AppMainPage.class));
+        });
+
+
+        Dexter.withContext(this)
+                .withPermissions(android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener()  {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                        startActivity(new Intent(getBaseContext(), AppMainPage.class));
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+
+                    }
+                })
+                .check();
+
+
+
+
+    }
+}
---
+++ Music_player_java/app/src/main/java/com/example/musicplaterjava/activity/AppMainPage.java
@@ -0,0 +1,21 @@
+package com.example.musicplaterjava.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.musicplaterjava.R;
+
+public class AppMainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_main_page);
+
+
+
+
+
+    }
+}
